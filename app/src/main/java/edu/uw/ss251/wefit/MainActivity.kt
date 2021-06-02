0f7991dabbb2214bc19e5af9bda6df82c3f5160e
ss251 <ss251@uw.edu>
2021-06-01 19:46:09 -0700

    init project

---
+++ app/src/main/java/edu/uw/ss251/wefit/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.uw.ss251.wefit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
